8f2d2cf6c7fb9b00872594f284efb34fb86a251a
Yoela O <65601594+BravishkaSkytano@users.noreply.github.com>
2023-12-14 21:14:31 -0500

    Fix install script

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Hyprland
 
 This repo contains all the config files for my hyprland setup on VoidLinux.
-I've also included an install script and an update script forfuture reference.
+I've also included an install script and an update script for future reference.
 Please be sure to check out [Makrennel's hyprland void-src repo](https://github.com/Makrennel/hyprland-void) for more info
-and refer to to [Jake@Linux](https://youtu.be/-tCdeY1Jqk0?si=w4CPaMrdaL6qeXDG) for the full install walkthrough
+and refer to to [Jake@Linux](https://youtu.be/-tCdeY1Jqk0?si=w4CPaMrdaL6qeXDG) for the full install tutorial
 
+Both scripts must be run with sudo privileges.
--- install-void.sh
+++ install-void.sh
@@ -17,7 +17,7 @@ cd void-packages
 ./xbps-src binary-bootstrap
 cd ..
 
-echo "CLoning hyprland-void from Makrennel..."
+echo "Cloning hyprland-void from Makrennel..."
 sleep 2
 git clone https://github.com/Makrennel/hyprland-void.git
 cd hyprland-void
@@ -31,15 +31,20 @@ cp -r srcpkgs/* ../void-packages/srcpkgs
 
 echo "Building and installing hyprland packages"
 cd ../void-packages
-./xbps-src pkg {hyprland,xdg-desktop-portal-hyprland,hyprland-protocols,hyprpaper}
-sudo xbps-install -R hostdir/binpkgs {hyprland,hyprland-protocols,xdg-desktop-portal-hyprland,hyprpaper}
+./xbps-src pkg hyprland
+./xbps-src pkg xdg-desktop-portal-hyprland
+./xbps-src pkg hyprland-protocols
+./xbps-src pkg hyprpaper
+sudo xbps-install -R hostdir/binpkgs hyprland hyprland-protocols xdg-desktop-portal-hyprland hyprpaper
 
 echo "Installing kitty, dbus, seatd, polkit elogind, mesa-dri"
 sleep 2
 sudo xbps-install kitty dbus seatd polkit elogind mesa-dri
 
 echo "Starting services..."
-sudo ln -s /etc/sv/{dbus,polkitd,seatd} /var/service
+sudo ln -s /etc/sv/dbus /var/service
+sudo ln -s /etc/sv/polkitd /var/service
+sudo ln -s /etc/sv/seatd /var/service
 
 echo "Assigning you to _seatd..."
 read -rp "What is your username?" $user
@@ -47,3 +52,4 @@ sudo usermod -aG _seatd $user
 
 echo "Done."
 echo "Congratulations on your new system!"
+echo "You should probably reboot, just in case ;)"
